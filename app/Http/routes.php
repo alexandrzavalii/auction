d0aa2badf7caa8b8f7d7aa6f1b59d13ba534b932
alexandrzavalii <alexandrzavalii@gmail.com>
2016-03-11 15:27:10 +0200

    back

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(array('https'), function () {
+Route::group(['middleware' => ['web']], function () {
 
 
 Route::get('/', ['as' => 'contact', 'uses' => 'WelcomeController@create']);
